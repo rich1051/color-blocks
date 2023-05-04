54475ffb48b7c3090783439bd672b154e4e3787e
Will Richardson <rich1051@umn.edu>
2023-05-04 17:30:02 -0500

    added jQuery click function to add/remove blocks

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
-console.log('🟥 🟦 🟩 🟨');
+$(document).ready(onReady);
+function onReady() {
+    console.log('jQuery is connected!');
+    $('#red-btn').on('click', appendRed);
+    $('#blue-btn').on('click', appendBlue);
+    $('#green-btn').on('click', appendGreen);
+    $('#yellow-btn').on('click', appendYellow);
+    $('#blocks').on('click', '.block', removeBlock);
+}
+// console.log('🟥 🟦 🟩 🟨');
+
+function appendRed() {
+    $('#blocks').append('<div class="block red-fill"><div>');
+}
+function appendBlue() {
+    $('#blocks').append('<div class="block blue-fill"><div>');
+}
+function appendGreen() {
+    $('#blocks').append('<div class="block green-fill"><div>');
+}
+function appendYellow() {
+    $('#blocks').append('<div class="block yellow-fill"><div>');
+}
+function removeBlock() {
+    $(this).remove();
+}
